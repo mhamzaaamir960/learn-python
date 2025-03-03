7c2a13bc81713457086f20bc89548bd1c94535ff
mhamzaaamir960 <mhamzaaamir960@gmail.com>
2025-03-03 16:17:26 +0500

    added comments folder

---
+++ 02_comments/Readme.md
@@ -0,0 +1,7 @@
+# Comments in Python
+
+* Comments are used to describe the logic in program or labels util other users understand the code easily.
+
+### Two types of comments in python
+- Single Line Comment (Start form #). It is easy to implement (shortcut press ctrl + /).
+- Multi Line Comment (""" description """). Use single or double line quotes
---
+++ 02_comments/comments.py
@@ -0,0 +1,27 @@
+
+# Single line commnet
+
+# Print a message
+print("Hello, world!")
+
+
+# Multi-line comment
+
+# Use single quote for multi-line comment
+
+'''
+This is a multi-line comment.
+It is used to write a comment that spans multiple lines.
+print a message
+'''
+print("Multi-line comment using single quote")
+
+
+# Use doulbe quote for multi-line comment
+
+"""
+This is a multi-line comment.
+It is used to write a comment that spans multiple lines.
+print a message.
+"""
+print("Multi-line comment using double quote")
